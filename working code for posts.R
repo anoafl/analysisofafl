d10b42c76d9da1976d85de91c2febab415c8a565
Analysis of AFL <analysisofafl@gmail.com>
2018-05-19 01:48:09 +1000

    push
    push

--- working code for posts.R
+++ working code for posts.R
@@ -78,8 +78,107 @@ library(tidyverse)
  #########---##########
 #####How to visualise betting data################
  library(gsheet) 
- url<-"https://docs.google.com/spreadsheets/d/1H3od0gyEkWj3icnTn55ywQ3YEQ--6xnhFl2AHI2QFdc/edit?usp=sharing"
+library(tidyverse)
+  url<-"https://docs.google.com/spreadsheets/d/1H3od0gyEkWj3icnTn55ywQ3YEQ--6xnhFl2AHI2QFdc/edit?usp=sharing"
  afl_bookies<-read.csv(text=gsheet2text(url, format='csv'), stringsAsFactors=FALSE)
-View(afl_bookies) 
+
+ head(afl_bookies)
+
+ afl_bookies%>%select(Date, Kick.Off..local., Home.Team, 
+                      Away.Team, Venue, Home.Score, Away.Score,
+                      Home.Line.Close, Away.Line.Close)%>%
+   mutate(home.margin=Home.Score-Away.Score)%>%
+   mutate(home.cover=home.margin+Home.Line.Close)%>%
+   mutate(away.margin=Away.Score - Home.Score)%>%
+   mutate(away.cover=away.margin+Away.Line.Close)
+
+    
+df<- afl_bookies%>%select(Date, Kick.Off..local., Home.Team, 
+                          Away.Team, Venue, Home.Score, Away.Score,
+                          Home.Line.Close, Away.Line.Close, Total.Score.Close)%>%
+  mutate(home.margin=Home.Score-Away.Score)%>%
+  mutate(home.cover=home.margin+Home.Line.Close)%>%
+  mutate(away.margin=Away.Score - Home.Score)%>%
+  mutate(away.cover=away.margin+Away.Line.Close)%>%
+  mutate(total_over=Home.Score + Away.Score - Total.Score.Close)
+
+df1<-select(df, Date,Kick.Off..local., Home.Team, 
+            Home.Line.Close,home.margin,home.cover,total_over)
+df1$h_a<-"home"
+
+df2<-select(df, Date,Kick.Off..local., Away.Team, 
+            Away.Line.Close,away.margin,away.cover,total_over)
+df2$h_a<-"away"
+
+colnames(df1)[3] <- "Team"
+colnames(df1)[4] <- "Line.Close"
+colnames(df1)[5] <- "margin"
+colnames(df1)[6] <- "cover"
+
+colnames(df2)[3] <- "Team"
+colnames(df2)[4] <- "Line.Close"
+colnames(df2)[5] <- "margin"
+colnames(df2)[6] <- "cover"
+
+df3<-rbind(df1,df2)
+df3$Date<-dmy(df3$Date)
+str(df3$Date)
+df3%>%arrange(Date)%>%
+  filter(Date>"2016-01-09")%>%
+  filter(Team=="Sydney")%>%
+mutate(games_number=row_number())%>%
+  ggplot(aes(y=total_over, x=Date,fill=h_a))+geom_col() +
+  ggtitle("Swans totals") +geom_text(aes(label=cover),vjust=-.5, size=2.5)+ 
+  theme_tufte()
  
- 
+
+df3%>%arrange(Date)%>%
+  mutate(games_number=row_number())%>%
+  ggplot(aes(y=cover, x=games_number,fill=h_a))+geom_col() 
+
+
+#############
+
+
+
+library(fitzRoy)
+library(lubridate)
+library(tidyverse)
+tips <- get_squiggle_data("tips")
+
+df<-tips%>%mutate(home.margin=ifelse(hteam==tip, margin,-margin))%>%
+  mutate(away.margin=ifelse(ateam==tip, margin,-margin)) %>%
+  select(source,date,correct,  hconfidence,hteam,
+         ateam,home.margin,away.margin,err ,tip,round, year)%>%
+  mutate(covered=ifelse(correct==1, err,-err))
+
+df1<-select(df,source, date, correct, hconfidence,hteam, home.margin, err, tip, round, year, covered )
+df1$H_A<-"HOME"
+df2<-select(df, source, date, correct, hconfidence, ateam, away.margin, err, tip, round, year, covered)
+df2$H_A<-"AWAY"
+colnames(df1)[5]<-"TEAM"
+
+colnames(df1)[6] <- "margin"
+
+colnames(df2)[5]<-"TEAM"
+
+colnames(df2)[6]<-"margin"
+
+df3<-rbind(df1,df2)
+str(df3$date)
+
+df3$date<-ymd_hms(df3$date)
+
+
+df3%>%arrange(date)%>%
+  filter(date>"2018-01-09")%>%
+  filter(round<10)%>%
+  filter(TEAM=="West Coast")%>%
+  ggplot(aes(y=margin, x=date,fill=H_A))+geom_col() +
+  ggtitle("West Coast")   +
+  theme_economist_white() +
+  theme(plot.title  = element_text(size =12),
+        axis.text = element_text(size = 6),
+        strip.text = element_text(size = 12))+
+  facet_wrap(~source)
+  
